87be51b57d145585b57007355d13e4e5672811bb
Carlos <carlosvenegass8c@gmail.com>
2024-04-01 18:26:29 -0300

    subiendo cambios

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Button from "./components/Button";
 import Description from "./components/Description";
 import Trial from "./components/Trial";
 import Image from "./components/Image";
-import Input from "./components/Input";
+//import input from "./components/input";
 import Title from "./components/Title";
 import Terms from "./components/Terms";
 import { useState } from "react";
@@ -13,22 +13,27 @@ function App() {
   const [email, setEmail] = useState("");
   const [isEmail, setIsEmail] = useState(true);
   const [password, setPassword] = useState("");
-  const [isInput, setIsInput] = useState(false);
+  const [error, setError] = useState({
+    name: false,
+    lastName: false,
+    email: false,
+    password: false,
+  });
 
   const validEmail = (email: string) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
   };
 
-  const handleChangeName = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeName = (event: any) => {
     const value = event.target.value;
     setName(value);
   };
-  const handleChangeLastName = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeLastName = (event: any) => {
     const value = event.target.value;
     setLastName(value);
   };
-  const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeEmail = (event: any) => {
     const value = event.target.value;
     setEmail(value);
     setIsEmail(validEmail(value));
@@ -36,30 +41,44 @@ function App() {
   console.log("email: ", email);
   console.log("isEmail: ", isEmail);
   console.log("validEmail: ", validEmail(email));
-  const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangePassword = (event: any) => {
     const value = event.target.value;
     setPassword(value);
   };
 
-  const handleButtonClick = () => {
-    if (
-      name.trim() === "" ||
-      lastName.trim() === "" ||
-      email.trim() === "" ||
-      password.trim() === ""
-    ) {
-      setIsInput(true);
-    } else if (validEmail(email) === true) {
-      setIsInput(false);
-      setIsEmail(true);
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    if (!name) {
+      setError({ ...error, name: true });
+      return;
     }
+    if (!lastName) {
+      setError({ ...error, lastName: true });
+      return;
+    }
+    if (!email && isEmail === true) {
+      setError({ ...error, email: true });
+      setIsEmail(true);
+      return;
+    }
+    if (!password) {
+      setError({ ...error, password: true });
+      return;
+    }
+    setError({
+      name: false,
+      lastName: false,
+      email: false,
+      password: false,
+    });
   };
 
   return (
     <div className="bg-bgColor h-screen relative">
-      <Image 
-      src="/bg-intro-desktop.png" 
-      srcSet="/bg-intro-desktop.png x1, /bg-intro-mobile.png 2"/>
+      <Image
+        src="/bg-intro-desktop.png"
+        srcSet="/bg-intro-desktop.png x1, /bg-intro-mobile.png 2"
+      />
       <div className="flex flex-cols justify-center items-center text-center relative z-10">
         <div className="mx-8 mt-8 w-1/4">
           <Title title="Learn to code by watching others" />
@@ -70,54 +89,74 @@ function App() {
           />
         </div>
         <div className="flex flex-col justify-center items-center mx-10 mt-8 w-1/2">
-            <div className="bg-bgColor w-90">
-              <Trial
+          <div className="bg-bgColor w-90">
+            <Trial
               segment="Try it free 7 days "
-              text="then $20/mo. thereafter"/>
+              text="then $20/mo. thereafter"
+            />
+          </div>
+          <form
+            onSubmit={handleSubmit}
+            className="bg-white pt-[18px] px-2 flex flex-col justify-center items-center text-center rounded-[5px] w-90"
+          >
+            <div>
+              <input
+                type="text"
+                id="name"
+                placeholder="First Name"
+                value={name}
+                onChange={handleChangeName}
+                className={`w-90 p-[16px] mb-1 font-semibold rounded-5 text-black leading-[26px] text-base font-['inherit'] border-2 border-solid focus:outline-none focus:ring-bgColor focus:border-bgColor ${
+                  error.name ? "border-red-500" : "border-gray-500"
+                }`}
+              />
             </div>
-            <div className="bg-white pt-[18px] px-2 flex flex-col justify-center items-center text-center rounded-[5px] w-90"> 
-            <Input
-              type="text"
-              id="name"
-              placeholder="First Name"
-              value={name}
-              onChange={handleChangeName}
-              isInput={isInput}
-              isEmail={isEmail}
+            <div>
+              <input
+                type="text"
+                id="lastName"
+                placeholder="Last Name"
+                value={lastName}
+                onChange={handleChangeLastName}
+                className={`w-90 p-[16px] mb-1 font-semibold rounded-5 text-black leading-[26px] text-base font-['inherit'] border-2 border-solid focus:outline-none focus:ring-bgColor focus:border-bgColor ${
+                  error.lastName ? "border-red-500" : "border-gray-500"
+                }`}
+              />
+            </div>
+            <div>
+              <input
+                type="email"
+                id="email"
+                placeholder="Email Name"
+                value={email}
+                onChange={handleChangeEmail}
+                className={`w-90 p-[16px] mb-1 font-semibold rounded-5 text-black leading-[26px] text-base font-['inherit'] border-2 border-solid focus:outline-none focus:ring-bgColor focus:border-bgColor ${
+                  error.email ? "border-red-500" : "border-gray-500"
+                }`}
+              />
+            </div>
+            <div>
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={handleChangePassword}
+                className={`w-90 p-[16px] mb-1 font-semibold rounded-5 text-black leading-[26px] text-base font-['inherit'] border-2 border-solid focus:outline-none focus:ring-bgColor focus:border-bgColor ${
+                  error.password ? "border-red-500" : "border-gray-500"
+                }`}
+              />
+            </div>
+            <Button
+              type="submit"
+              text="Claim your free trial"
+              onClick={handleSubmit}
             />
-            <Input
-              type="text"
-              id="lastName"
-              placeholder="Last Name"
-              value={lastName}
-              onChange={handleChangeLastName}
-              isInput={isInput}
-              isEmail={isEmail}
-            />
-            <Input
-              type="email"
-              id="email"
-              placeholder="Email Name"
-              value={email}
-              onChange={handleChangeEmail}
-              isInput={isInput}
-              isEmail={isEmail}
-            />
-            <Input
-              type="password"
-              id="password"
-              placeholder="Password"
-              value={password}
-              onChange={handleChangePassword}
-              isInput={isInput}
-              isEmail={isEmail}
-            />
-            <Button text="Claim your free trial" onClick={handleButtonClick} />
             <Terms
               text="By clicking the button, you are agreeing to our"
               segment="Terms and Services"
             />
-          </div>
+          </form>
         </div>
       </div>
     </div>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,7 +3,12 @@ interface Props {
   id: string;
   placeholder: string;
   value: string;
-  isInput: boolean;
+  isInput: {
+    name: boolean;
+    lastName: boolean;
+    email: boolean;
+    password: boolean;
+  };
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   isEmail: boolean;
 }
@@ -14,7 +19,7 @@ function Input(props: Props) {
     <div>
       <input
         className={`w-90 p-[16px] mb-1 font-semibold rounded-5 text-black leading-[26px] text-base font-['inherit'] border-2 border-solid focus:outline-none focus:ring-bgColor focus:border-bgColor ${
-          props.isInput ? "border-red-500" : "border-bgColor"
+          props.isInput ? "border-red-500" : "border-gray-500"
         }`}
         type={props.type}
         id={props.id}
@@ -22,16 +27,16 @@ function Input(props: Props) {
         value={props.value}
         onChange={props.onChange}
       />
-      {props.isInput && props.id === "name" && props.value === "" && (
+      {props.isInput.name && props.id === "name" && props.value === "" && (
         <p className="text-red-500 my-2">First Name cannot be empty</p>
       )}
-      {props.isInput && props.id === "lastName" && props.value === "" && (
+      {props.isInput.lastName && props.id === "lastName" && props.value === "" && (
         <p className="text-red-500 my-2">Last Name cannot be empty</p>
       )}
-      {props.isInput && props.id === "email" && (props.value === "" || !props.isEmail) &&(
+      {props.isInput.email && props.id === "email" && (props.value === "" || !props.isEmail) &&(
         <p className="text-red-500 my-2">Looks like this is not an email</p>
       )}
-      {props.isInput && props.id === "password" && props.value === "" && (
+      {props.isInput.password && props.id === "password" && props.value === "" && (
         <p className="text-red-500 my-2">Password cannot be empty</p>
       )}
     </div>
